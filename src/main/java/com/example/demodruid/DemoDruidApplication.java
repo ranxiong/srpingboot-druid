2369d81be90234b1a95e4974439bf93c85198109
ranxiong <390630235@qq.com>
2018-11-06 23:55:12 +0800

    监控配置提交

---
+++ src/main/java/com/example/demodruid/DemoDruidApplication.java
@@ -0,0 +1,12 @@
+package com.example.demodruid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDruidApplication.class, args);
+    }
+}
